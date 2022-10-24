a93eb5b9660e332ee342ba0ed700702d1d3cbfa0
Yuri augusto Santos nunes <ysantos-@c2r3s1.42lisboa.com>
2022-10-24 01:52:04 +0100

    start

---
+++ push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysantos- <ysantos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 01:11:54 by ysantos-          #+#    #+#             */
+/*   Updated: 2022/10/24 01:30:15 by ysantos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "ft_printf/ft_printf.h"
+
+
+#endif
