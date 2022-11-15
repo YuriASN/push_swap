f44e3fa1aa3894127edc3c61a002df8688af52e9
YuriASN <yuriasn@hotmail.com>
2022-11-15 21:09:02 +0000

    Need to fix order_b

--- push_order.c
+++ push_order.c
@@ -6,14 +6,15 @@
 /*   By: ysantos- <ysantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 17:07:21 by ysantos-          #+#    #+#             */
-/*   Updated: 2022/11/14 20:33:07 by ysantos-         ###   ########.fr       */
+/*   Updated: 2022/11/15 20:51:11 by ysantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 /* Choose where the A has to pushed at B. */
-static int	push_where(t_stk *stk_a, t_stk *stk_b)
+static void	push_where(t_stk *stk_a, t_stk *stk_b)
 {
+//printf("\tno push where%s\n", CRESET);getchar();
 	if (stk_a->nxt->value < stk_b->nxt->value
 		&& stk_a->nxt->value >= stk_b->nxt->nxt->value)
 	{
@@ -35,13 +36,11 @@ static int	push_where(t_stk *stk_a, t_stk *stk_b)
 		push_stk(stk_a, stk_b, 2);
 		rotate_stk(stk_b, 2);
 	}
-//printf("\t%sPush where did nothing%s\n", RED, CRESET);
-	return (0);
 }
-		
+
 static int	order1(t_stk *stk_a, t_stk *stk_b)
 {
-//printf("%sStart Order1%s\n", YEL, CRESET); 
+//printf("%sStart Order1%s\n", YEL, CRESET); getchar();
 	while (stk_a->nxt->value > (stklast(stk_a))->value)
 		rotate_rev(stk_a, 1);
 	if (stk_a->nxt->value > stk_a->nxt->nxt->value && stk_b->nxt->value < stk_b->nxt->nxt->value)
@@ -51,8 +50,8 @@ static int	order1(t_stk *stk_a, t_stk *stk_b)
 	else if (stk_b->nxt->value < stk_b->nxt->nxt->value)
 		swap_stk(stk_b, 2);
 	else
-		push_where(stk_a, stk_b);
-//printf("\t%sOrder1 did nothing%s\n", RED, CRESET);
+	{	//printf("\t%sOrder1 did nothing%s\n", RED, CRESET);
+		push_where(stk_a, stk_b);}
 	return (0);
 }
 
@@ -60,47 +59,44 @@ static int	order1(t_stk *stk_a, t_stk *stk_b)
 static void	order_a(t_stk *stack)
 {
 //printf("%sinto order A\n%s", YEL, CRESET);
-	while (stack->nxt && (get_lowest(stack->nxt) > 0 || !check_order(stack)))
+	while (!check_order(stack))
 	{
-		if (stack->nxt->value > stack->nxt->nxt->value)
+		if (stack->nxt->value > stack->nxt->nxt->value && get_lowest(stack->nxt) != 1)
 			swap_stk(stack, 1);
-		else if (get_lowest(stack->nxt) <= stksize(stack->nxt) / 2)
-			rotate_stk(stack, 1);
-		else
-			rotate_rev(stack, 1);
-		
+		rotate_stk(stack, 1);
+//print_list(stack);
+//getchar();
 	}
 }
 
+/* **************************************************  FIX IT!  ************************************************** */
 /* Get only stack b in order when a second stack is alredy ordered */
 static void	order_b(t_stk *stack)
 {
 //printf("%s\tinto order B\n%s", YEL, CRESET);
 	while (!check_order_r(stack))
 	{
-		if (stack->nxt->value < stack->nxt->nxt->value)
+		if (stack->nxt->value < stack->nxt->nxt->value && get_lowest(stack->nxt) < stksize(stack->nxt))
 			swap_stk(stack, 2);
-		else if (stack->nxt->value < (stklast(stack->nxt))->value)
-			rotate_rev(stack, 2);
-		else
-			rotate_stk(stack, 2);
+		rotate_stk(stack, 2);
+//getchar();
 	}
 }
+/* *************************************************************************************************************** */
 
-
+/* Put lowest number at first to be pushed to b. */
 static void	start_stka(t_stk *stk_a)
 {
-//printf("\tno start stka\033\n");
 	int	z;
 
 	z = get_lowest(stk_a->nxt);
 	if (z >= stksize(stk_a->nxt) / 2)
-	{//printf("\033[93mz (%i) maior que meio \033[m\n", z);
+	{
 		while (++z < stksize(stk_a->nxt))
 			rotate_rev(stk_a, 1);
 	}
 	else if (z < stksize(stk_a->nxt) / 2)
-	{//printf("\033[93mz (%i) menor que meio\033[m\n", z);
+	{
 		while (++z < stksize(stk_a->nxt) / 2)
 			rotate_stk(stk_a, 1);
 	}
@@ -116,12 +112,11 @@ void	get_order(t_stk *stk_a, t_stk *stk_b)
 //printf("%sGet order\n%slist a:%s\n", YEL, CYN, CRESET); 	print_list(stk_a);		//printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
 	if (check_order(stk_a) && check_order_r(stk_b))
 	{
-//printf("\n\nAlready on order\n");
 		if (stk_b->nxt)
 			b_to_a(stk_a, stk_b);
 		exit (0);
 	}
-	if (stksize(stk_a->nxt) > 4)
+	if (stksize(stk_a->nxt) > 7)
 	{
 		start_stka(stk_a);
 		push_stk(stk_a, stk_b, 1);
@@ -132,15 +127,13 @@ void	get_order(t_stk *stk_a, t_stk *stk_b)
 		order_a(stk_a);
 		exit (0);
 	}
-//printf("\e[93m\tout of if\033[m\n");
 	while ((!check_order(stk_a) || !check_order_r(stk_b)) || stk_a->nxt->value < stk_b->nxt->value)
 	{
-//printf("\t\t\t%sWhile no order%s\nlist a:%s\n", RED, CYN, CRESET); 	print_list(stk_a);		//printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
+//printf("\t\t\t%sWhile no order%s\nlist a:%s\n", RED, CYN, CRESET);// 	print_list(stk_a);		//printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
 //getchar();
-		
 		order1(stk_a, stk_b);
-		if (stksize(stk_a->nxt) < 5 && !check_order(stk_a))
-			order_a(stk_a);
+		if (stksize(stk_a->nxt) < 8 && !check_order(stk_a))
+			{order_a(stk_a); print_list(stk_a);}
 		if (!check_order_r(stk_b))
 			order_b(stk_b);
 		if (stk_a->nxt->value < stk_b->nxt->value && stksize(stk_a->nxt) > 4)
@@ -152,5 +145,8 @@ void	get_order(t_stk *stk_a, t_stk *stk_b)
 	if (stk_b->nxt)
 		b_to_a(stk_a, stk_b);
 	if (check_order(stk_a) && !stk_b->nxt){//printf("\t\t\e[0;32mEND WITHOUT LEAKS!\e[0m\n"); //printf("%slist a:%s\n", CYN, CRESET); 	print_list(stk_a);		//printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
-		exit (0);			}
+		exit (0);	}
 }
+
+
+//Change all exit for a funtion that free the stack and exit
