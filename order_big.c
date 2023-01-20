cf980e11febb392b56307d693b6542ff946bdac0
YuriASN <yuriasn@hotmail.com>
2023-01-20 02:31:49 +0000

    Fixed lis_on_a where j<=lissize was blocking the stack to finish as order position

--- order_big.c
+++ order_big.c
@@ -1,7 +1,7 @@
 #include "push_swap.h"
 
 /*	Leave A only with LIS numbers. */
-static void	lis_on_a(t_stk *stk_a, t_stk *stk_b, int *lis, int lis_size)
+static void	lis_on_a(t_stk *stk_a, t_stk *stk_b, int *lis)
 {
 	int	i;
 	int	j;
@@ -12,8 +12,8 @@ static void	lis_on_a(t_stk *stk_a, t_stk *stk_b, int *lis, int lis_size)
 	size_a = stksize(stk_a->nxt);
 	while (++i < size_a)
 	{
-printf("i = %i\t lis[%i] = %i\n", i, j, lis[j]);
-		if (j <= lis_size && i != lis[j])
+//printf("i = %i\t lis[%i] = %i\t", i, j, lis[j]);
+		if (i != lis[j])
 		{
 			push_stk(stk_a, stk_b, 1);
 		}
@@ -22,14 +22,14 @@ printf("i = %i\t lis[%i] = %i\n", i, j, lis[j]);
 			++j;
 			rotate_stk(stk_a, 1);
 		}
-printf("%slist a:%s\n", CYN, CRESET); 	print_list(stk_a);		printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
+//printf("%slist a:%s\n", CYN, CRESET); 	print_list(stk_a);		printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
 	}
 	free(lis);
 }
 
 /*	Returns array with index of Longest Increasing Subsequence numbers.
 	And attributes the size to lis_size */
-static int	*get_lis(t_stk *stk_a, int *lis, int *lis_size)
+static void	get_lis(t_stk *stk_a, int *lis, int *lis_size)
 {
 	int		i;
 	int		sv;
@@ -54,8 +54,7 @@ static int	*get_lis(t_stk *stk_a, int *lis, int *lis_size)
 		}
 		tmp = tmp->nxt;
 	}
-printf("lis_size = %i\n", *lis_size); printf("lis = "); int o = -1;  while (++o <= *lis_size){printf("[%i]%i, ", o, lis[o]);} printf("\n\n");
-	return (lis);
+//printf("lis_size = %i\n", *lis_size); printf("lis = "); int o = -1;  while (++o <= *lis_size){printf("[%i]%i, ", o, lis[o]);} printf("\n\n");
 }
 
 /* Start ordering lists with  5 arguments or more than 7 */
@@ -70,16 +69,15 @@ void	order_big(t_stk *stk_a, t_stk *stk_b)
 	lis_size = -1;
 	lis = ft_calloc(stksize(stk_a->nxt), sizeof(int *));
 	lowest_pos = get_lowest(stk_a->nxt);
-printf("lowest_pos = %i\n", lowest_pos);
 	if (lowest_pos <= stksize(stk_a->nxt) / 2)
 		while (++i != lowest_pos)
 			rotate_stk(stk_a, 1);
 	else
 		while (lowest_pos++ != stksize(stk_a->nxt))
 			rotate_rev(stk_a, 1);
-printf("\nBefore LIS\n%slist a:%s\n", CYN, CRESET); 	print_list(stk_a);		printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
+//printf("\nBefore LIS\n%slist a:%s\n", CYN, CRESET); 	print_list(stk_a);		printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
 	get_lis(stk_a, lis, &lis_size);
-	lis_on_a(stk_a, stk_b, lis, lis_size);
+	lis_on_a(stk_a, stk_b, lis);
 printf("\nafter LIS\n%slist a:%s\n", CYN, CRESET); 	print_list(stk_a);		printf("%slist b:%s\n", CYN, CRESET);	print_list(stk_b);
 	less_moves(stk_a, stk_b);
 }
